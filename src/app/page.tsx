87d3dcc6750c43cfb09bcedfad42021e73a9f372
DainJung <wjdekdls3693@gmail.com>
2025-05-11 02:44:39 +0900

    ui: 주요 후보 카드 수 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
                 </span>
             </div>
           </div>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-8">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-8">
             {mainCandidates.map((candidate) => (
               <div key={candidate.id}>
                 <CandidateCard candidate={candidate} />
